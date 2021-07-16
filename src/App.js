e3de1182f65577db4f1391e3f761b24a1701f00e
Marshall3572 <944797458@qq.com>
2021-07-16 17:29:41 +0800

    Router配置成功

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route} from 'react-router-dom'
+
+import './App.css'
 import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+import {Home} from './pages/Home'
+import {History} from './pages/History'
+import {About} from './pages/About'
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header/>
+      <Switch>
+        {/*exact表示严格匹配模式，加了后代就无法匹配自己*/}
+        <Route path="/" exact component={Home}/>
+        <Route path="/history" component={History}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Logo from '../logo.svg'
+import {Link} from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <div>
+      <img src={Logo}/>
+      <nav>
+        <Link to='/'>首页</Link>
+        <Link to='/history'>上传历史</Link>
+        <Link to='/about'>关于我</Link>
+      </nav>
+    </div>
   )
 }
 
