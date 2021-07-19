362d767c4254ffc9ddb18361e93cf879ce2863cf
Marshall3572 <944797458@qq.com>
2021-07-19 20:14:36 +0800

    多种方法进行antd表单验证

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,14 +3,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Component = () => {
-  const onFinish = (values) => {
-    console.log('Success:', values)
-  }
-
   const Wrapper = styled.div`
     max-width: 600px;
     margin: 30px auto;
-    box-shadow: 2px 2px 4px rgba(0,0,0,0.2\);
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2\);
     border-radius: 4px;
     padding: 20px;
   `
@@ -19,9 +15,28 @@ const Component = () => {
     margin-bottom: 30px;
   `
 
+  const onFinish = (values) => {
+    console.log('Success:', values)
+  }
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   }
+
+
+  const validateUsername = (rule, value) => {
+    if (/\W/.test(value)) return Promise.reject('不能出现字母数字下划线以外的字符')
+    if (value.length < 3) return Promise.reject('用户名长度不能小于3')
+    if (value.length > 10) return Promise.reject('用户名长度不能大于10')
+    Promise.resolve()
+  }
+  // 箭头函数后加()是因为返回了一个对象，不加会被理解为函数体
+  const validateConform = ({getFieldValue}) => ({
+    validator(rule, value){
+      if(getFieldValue('password') === value)
+        return Promise.resolve();
+       return Promise.reject('两次密码不一致')
+    }
+  })
 
   return (
     <Wrapper>
@@ -29,10 +44,10 @@ const Component = () => {
       <Form
         name="basic"
         labelCol={{
-          span: 4,
+          span: 6,
         }}
         wrapperCol={{
-          span: 20,
+          span: 18,
         }}
         initialValues={{
           remember: true,
@@ -47,7 +62,9 @@ const Component = () => {
             {
               required: true,
               message: '请输入用户名',
-            },
+            }, {
+              validator: validateUsername
+            }
           ]}
         >
           <Input/>
@@ -59,7 +76,13 @@ const Component = () => {
             {
               required: true,
               message: '请输入密码',
-            },
+            }, {
+              min: 4,
+              message: '密码最少4位数'
+            }, {
+              max: 10,
+              message: '密码最多十位数'
+            }
           ]}
         >
           <Input.Password/>
@@ -67,12 +90,13 @@ const Component = () => {
 
         <Form.Item
           label="确认密码"
-          name="password"
+          name="confirmPassword"
           rules={[
             {
               required: true,
               message: '请再次确认密码',
             },
+            validateConform
           ]}
         >
           <Input.Password/>
@@ -80,8 +104,8 @@ const Component = () => {
 
         <Form.Item
           wrapperCol={{
-            offset: 4,
-            span: 20,
+            offset: 6,
+            span: 18,
           }}
         >
           <Button type="primary" htmlType="submit">
