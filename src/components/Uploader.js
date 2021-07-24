72a2bc3614cffc6bf58f5e2445693963112de1fe
Marshall3572 <944797458@qq.com>
2021-07-24 18:24:13 +0800

    上传结果展示

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -3,9 +3,22 @@ import {useStores} from '../stores'
 import {observer} from 'mobx-react'
 import {InboxOutlined} from '@ant-design/icons'
 import {Upload, message} from 'antd'
+import styled from 'styled-components'
 
 const {Dragger} = Upload
 
+const Result = styled.div`
+  margin-top: 30px;
+  border: 1px dashed #ccc;
+  padding: 20px;
+`
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`
+const Image = styled.img`
+  max-width: 300px;
+`
 
 const Component = observer(() => {
   const {ImageStore, UserStore} = useStores()
@@ -15,16 +28,16 @@ const Component = observer(() => {
     beforeUpload: file => {
       ImageStore.setFile(file)
       ImageStore.setFilename(file.name)
-      if(UserStore.currentUser == null){
-        message.warning('请先登陆再上传')
-      }else{
+      // if (UserStore.currentUser == null) {
+      //   message.warning('请先登陆再上传')
+      // } else {
         ImageStore.upload()
           .then((serverFile) => {
               console.log('上传成功')
               console.log(serverFile)
             }
           ).catch(() => console.log('上传失败'))
-      }
+      // }
       return false
     }
   }
@@ -41,12 +54,28 @@ const Component = observer(() => {
           band files
         </p>
       </Dragger>
-      <div>
-        <h1>上传结果</h1>
-        {ImageStore.serverFile ? <div>{ImageStore.serverFile.attributes.url.attributes.url}</div> : null}
-      </div>
+
+      {
+        ImageStore.serverFile ?
+          <Result>
+            <H1>上传结果</H1>
+            <dl>
+              <dt>线上地址</dt>
+              <dd><a target="_blank" href={ImageStore.serverFile.attributes.url.attributes.url}>{ImageStore.serverFile.attributes.url.attributes.url}</a></dd>
+              <dt>文件名</dt>
+              <dd>{ImageStore.filename}</dd>
+              <dt>图片预览</dt>
+              <dd>
+                <Image src={ImageStore.serverFile.attributes.url.attributes.url}/>
+              </dd>
+              <dt>更多尺寸</dt>
+              <dd>...</dd>
+            </dl>
+          </Result> : null
+      }
     </div>
   )
-})
+}
+)
 
 export default Component
